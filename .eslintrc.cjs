6cee0f038e6b855d4dad82d71fca0d47a1fddcf2
Sankalp Mukim <sankalpmukim@gmail.com>
2023-09-14 22:19:56 +0530

    fix: remove type import consistency rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,13 +16,6 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
 
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
 };
