fc93714ff7bd822ff3426c8c9d687cf337d51163
Sankalp Mukim <sankalpmukim@gmail.com>
2023-09-16 19:24:14 +0530

    chore: update given schema for new changes

--- scripts/convert.ts
+++ scripts/convert.ts
@@ -2,4 +2,4 @@ import schema from "../src/schema";
 import { zodToJsonSchema } from "zod-to-json-schema";
 
 const jsonSchema = zodToJsonSchema(schema, "mySchema");
-console.log(JSON.stringify(jsonSchema, null, 2));
+console.log(JSON.stringify(jsonSchema));
--- src/schema.ts
+++ src/schema.ts
@@ -3,17 +3,30 @@ import { z } from "zod";
 export const defaultValue = `
 {
   "$schema": "https://cryptichuntjson.sankalpmukim.dev/schema.json",
-  "name": "My Hunt",
-  "description": "My Hunt Description",
-  "phase": 1,
+  "name": "Phase 4-6",
+  "description": {
+    "Question": "QuestionName", 
+    "Difficulty": "Easy|Medium|Hard", 
+    "Authors": [
+      "Author 1",
+      "Author 2"
+    ]
+  },
+  "phase": 4,
   "isQR": true,
+  "qrLink": [
+    "http://with.acmvit.in/phase4-6-erqweasgdterg"
+  ],
   "question": {
-    "title": "My Question",
-    "description": "My Question Description",
-    "answer": "My Answer",
-    "pointsAwarded": 10,
-    "costofHint": 5,
-    "hint": "My Hint"
+    "title": "This is the name shown to people",
+    "description": "This is the description shown to people",
+    "answer": "The expected answer",
+    "pointsAwarded": 100,
+    "costofHint": 50,
+    "hint": "ASCII 85",
+    "images": [
+        "https://cloudfront.net/questionImage.jpg"
+    ]
   }
 }
 `.trim();
@@ -23,13 +36,16 @@ const mySchema = z
     $schema: z
       .literal("https://cryptichuntjson.sankalpmukim.dev/schema.json")
       .optional(),
-    name: z.string({
-      required_error: "Name is required",
-      invalid_type_error: 'Name must be a string try using `"`',
-    }),
-    description: z.string({
-      required_error: "Description is required",
-      invalid_type_error: "Description must be a string",
+    name: z
+      .string({
+        required_error: "Name is required",
+        invalid_type_error: 'Name must be a string try using `"`',
+      })
+      .startsWith("Phase "),
+    description: z.object({
+      Question: z.string(),
+      Difficulty: z.enum(["Easy", "Medium", "Hard"]),
+      Authors: z.array(z.string()),
     }),
     phase: z
       .number({
@@ -42,6 +58,16 @@ const mySchema = z
       required_error: "isQR is required",
       invalid_type_error: "isQR must be a boolean. try `true` or `false`",
     }),
+    qrLink: z.array(
+      z
+        .string({
+          required_error: "QR Link is required",
+          invalid_type_error: "QR Link must be a string",
+        })
+        .url({
+          message: "QR Link must be a valid url",
+        }),
+    ),
     question: z.object(
       {
         title: z.string({
@@ -56,18 +82,43 @@ const mySchema = z
           required_error: "Question answer is required",
           invalid_type_error: "Question answer must be a string",
         }),
-        pointsAwarded: z.number({
-          required_error: "Question points awarded is required",
-          invalid_type_error: "Question points awarded must be a number",
-        }),
+        pointsAwarded: z
+          .literal(100, {
+            required_error: "Question points awarded is required",
+            invalid_type_error: "Question points awarded must be a number",
+          })
+          .or(
+            z.literal(200, {
+              required_error: "Question points awarded is required",
+              invalid_type_error: "Question points awarded must be a number",
+            }),
+          )
+          .or(
+            z.literal(350, {
+              required_error: "Question points awarded is required",
+              invalid_type_error: "Question points awarded must be a number",
+            }),
+          ),
         costofHint: z.number({
           required_error: "Question cost of hint is required",
           invalid_type_error: "Question cost of hint must be a number",
         }),
-        hint: z.string({
-          required_error: "Question hint is required",
-          invalid_type_error: "Question hint must be a string",
-        }),
+        hint: z
+          .string({
+            required_error: "Question hint is required",
+            invalid_type_error: "Question hint must be a string",
+          })
+          .nullable(),
+        images: z.array(
+          z
+            .string({
+              required_error: "Question images is required",
+              invalid_type_error: "Question images must be a string",
+            })
+            .url({
+              message: "Question images must be a valid url",
+            }),
+        ),
       },
       {
         required_error: "Question is required",
